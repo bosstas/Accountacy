f1b1c61f66749f7bfd320e69e46d8b5fbb67a577
bosstas <56200998+bosstas@users.noreply.github.com>
2020-06-27 22:12:55 +0300

    Update main.js

--- accountacy/main.js
+++ accountacy/main.js
@@ -1,3 +1,14 @@
 $(function(){
-	$('#data').append('<img src=button.jpg>')
-})
+	$(".tip").change(function(){
+		var id=$(".tip").val();
+
+		$.ajax({
+			url: "index.php",
+			type: "POST",
+			data: {id:id},
+			success: function(data){
+				$(".status").html(data);
+			}
+		});
+	});
+});
